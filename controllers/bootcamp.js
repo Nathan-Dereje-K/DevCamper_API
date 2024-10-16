a343c76819d389c56f09fc59778ab0a726f420cc
Nathan <natidare61@gmail.com>
2024-10-16 15:28:58 +0300

    "Added try-catch blocks and error handling to Bootcamp controller methods"

--- controllers/bootcamp.js
+++ controllers/bootcamp.js
@@ -6,8 +6,16 @@ const Bootcamp = require("../models/Bootcamp");
 // @Access     Public
 
 exports.getBootcamps = async (req, res, next) => {
-  const bootcamps = await Bootcamp.find(req.body);
-  res.status(200).json({ success: true, msg: { bootcamps } });
+  try {
+    const bootcamp = await Bootcamp.find(req.body);
+    res
+      .status(200)
+      .json({ success: true, count: bootcamp.length, msg: { bootcamp } });
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+    });
+  }
 };
 
 // @Desc       get a specific Bootcamp
@@ -15,12 +23,22 @@ exports.getBootcamps = async (req, res, next) => {
 // @Access     Public
 
 exports.getBootcamp = async (req, res, next) => {
-  const id = req.params.id;
-  const bootcamp = await Bootcamp.findById(id);
-  res.status(200).json({
-    success: true,
-    msg: bootcamp,
-  });
+  try {
+    const id = req.params.id;
+    const bootcamp = await Bootcamp.findById(id);
+    if (!bootcamp) {
+      return res.status(404).json({ success: false });
+    }
+    res.status(200).json({
+      success: true,
+      msg: bootcamp,
+    });
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+      msg: err,
+    });
+  }
 };
 
 // @Desc       create Bootcamp
@@ -28,12 +46,18 @@ exports.getBootcamp = async (req, res, next) => {
 // @Access     Private
 
 exports.createBootcamp = async (req, res, next) => {
-  const bootcamp = await Bootcamp.create(req.body);
-  res.status(201).json({
-    success: true,
-    msg: `Bootcamp created successfully`,
-    data: { bootcamp },
-  });
+  try {
+    const bootcamp = await Bootcamp.create(req.body);
+    res.status(201).json({
+      success: true,
+      msg: `Bootcamp created successfully`,
+      data: { bootcamp },
+    });
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+    });
+  }
 };
 
 // @Desc       Update Bootcamp
@@ -41,17 +65,24 @@ exports.createBootcamp = async (req, res, next) => {
 // @Access     Private
 
 exports.updateBootcamp = async (req, res, next) => {
-  const id = req.params.id;
-  const bootcamp = await Bootcamp.findByIdAndUpdate(id);
-
-  const updatedBootcamp = await Bootcamp.create(req.body);
-  res.status(201).json({
-    msg: "Updated",
-  });
-
-  res
-    .status(200)
-    .json({ success: true, msg: `Update bootcamp ${req.params.id} ` });
+  try {
+    const id = req.params.id;
+    const bootcamp = await Bootcamp.findByIdAndUpdate(id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!bootcamp) {
+      return res.status(404).json({ success: false });
+    }
+    res.status(200).json({
+      success: true,
+      data: bootcamp,
+    });
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+    });
+  }
 };
 
 // @Desc       Delete Bootcamp
@@ -59,11 +90,16 @@ exports.updateBootcamp = async (req, res, next) => {
 // @Access     Private
 
 exports.deleteBootcamp = async (req, res, next) => {
-  const id = req.params.id;
-  const bootcamp = await Bootcamp.findByIdAndDelete(id);
-  res.status(200).json({
-    success: true,
-    msg: `Bootcamp deleted successfully`,
-    msg: bootcamp,
-  });
+  try {
+    const id = req.params.id;
+    await Bootcamp.findByIdAndDelete(id);
+    res.status(200).json({
+      success: true,
+      msg: `Bootcamp deleted successfully`,
+    });
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+    });
+  }
 };

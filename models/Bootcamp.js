f4a18283265b53ec9415c39233c42d4302ff8b1e
Nathan <natidare61@gmail.com>
2024-10-20 14:36:13 +0300

    "Added geocoder functionality to Bootcamp model and created geocoder instance with OpenCage API key"

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -104,5 +104,21 @@ BootcampSchema.pre("save", function () {
   this.slug = slugify(this.name, { lower: true });
 });
 // Geocoder and create location field
+BootcampSchema.pre("save", async function (next) {
+  const loc = await geocoder.geocode(this.address);
+  this.location = {
+    type: "Point",
+    coordinates: [loc[0].longitude, loc[0].latitude],
+    formattedAddress: loc[0].formattedAddress,
+    street: loc[0].streetName,
+    city: loc[0].city,
+    state: loc[0].stateCode,
+    zipcode: loc[0].zipcode,
+    country: loc[0].countryCode,
+  };
 
+  // Do not save address in DB
+  this.address = undefined;
+  next();
+});
 module.exports = mongoose.model("Bootcamp", BootcampSchema);
--- ultils/geocoder.js
+++ ultils/geocoder.js
@@ -1,11 +1,15 @@
 const NodeGeocoder = require("node-geocoder");
 
+// Set up the options with the correct provider and API key
 const options = {
-  provider: process.env.GEOCODER_PROVIDER,
+  provider: "opencage", // Correct provider name in lowercase
   httpAdapter: "https",
-  apiKey: process.env.GEOCODER_API_KEY,
+  apiKey: "b439a2e5a7d0466788e35d7b1450152e", // Hardcoded OpenCage API key
   formatter: null,
 };
 
+// Create the geocoder instance
 const geocoder = NodeGeocoder(options);
+
+// Export the geocoder to use it in other parts of your application
 module.exports = geocoder;
